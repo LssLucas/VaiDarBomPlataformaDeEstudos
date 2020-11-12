09c015b460e12fb0f7c21211e7bccd4bb013afa8
Jefferson Russo Silva <jefferson.russo@Outlook.com>
2020-11-12 00:14:04 -0300

    a

--- BlogPessoal/src/app/model/User.ts
+++
@@ -1,8 +0,0 @@
-export class User{
-    public id: number
-    public usuario: string
-    public nome: string
-    public email: string
-    public senha: string
-    public tipoUsuario: number
-}
--- BlogPessoal/src/app/model/UserLogin.ts
+++
@@ -1,8 +0,0 @@
-export class UserLogin{
-    public usuario: string
-    public nome: string
-    public email: string
-    public senha: string
-    public token: string
-    
-}
