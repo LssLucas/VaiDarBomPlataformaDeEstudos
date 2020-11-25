8a262309a827cf6c863a36430077776153b10e6a
Jefferson Russo Silva <jefferson.russo@Outlook.com>
2020-11-24 21:22:04 -0300

    tentei fazer os comentarios :(

--- vaiDarBom-PlataformaEstudos/src/app/cadastro/cadastro.component.ts
+++ vaiDarBom-PlataformaEstudos/src/app/cadastro/cadastro.component.ts
@@ -13,6 +13,7 @@ export class CadastroComponent implements OnInit {
 
   user: User = new User()
   senha: string
+  ft : string
 
   constructor(
     private authService: AuthService,
@@ -24,11 +25,20 @@ export class CadastroComponent implements OnInit {
 
   cadastrar(){
     if( this.senha === this.user.senha ) {
+
+      if(this.user.foto == "" || this.user.foto == null)
+      { 
+        this.ft = 'https://fopiess.org.br/wp-content/uploads/2018/01/default1.jpg'
+        this.user.foto = this.ft
+      }
+
       this.authService.cadastrar(this.user).subscribe((resp: User) => {
         this.user = resp
         this.router.navigate(['/login'])
+
         this.alerta.showAlerSucess('Usuário cadastrado com sucesso')
       })
+      
     } else{
       this.alerta.showAlertDanger('Sua senha não confere')
     }
